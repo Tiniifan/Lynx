4de5bce34fdb203ee7c4e38ea05b62fbff1a9413
Tinifan <30804632+Tiniifan@users.noreply.github.com>
2025-11-08 21:09:39 +0100

    Add GameType enum and integrate it into ProjectData model
    - Created new enum 'GameType' defining supported games: IEGO, IEGOCS, and IEGOGalaxy.
    - Integrated 'GameType' into the ProjectData class via a new 'Game' property.

---
+++ Lynx/Models/GameType.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lynx.Models
+{
+    /// <summary>
+    /// Defines the supported game types for the project.
+    /// </summary>
+    public enum GameType
+    {
+        /// <summary>
+        /// Inazuma Eleven GO.
+        /// </summary>
+        IEGO,
+
+        /// <summary>
+        /// Inazuma Eleven GO Chrono Stones.
+        /// </summary>
+        IEGOCS,
+
+        /// <summary>
+        /// Inazuma Eleven GO Galaxy.
+        /// </summary>
+        IEGOGalaxy
+    }
+}
