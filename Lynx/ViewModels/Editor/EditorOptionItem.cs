9bb884638db4b2eeac616e512209d60af57b6083
Tinifan <30804632+Tiniifan@users.noreply.github.com>
2025-11-09 09:32:05 +0100

    Create EditorOptionItem.cs

---
+++ Lynx/ViewModels/Editor/EditorOptionItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lynx.ViewModels.Editor
+{
+    public class EditorOptionItem
+    {
+        public string DisplayName { get; set; }
+        public EditorOption Option { get; set; }
+    }
+}
